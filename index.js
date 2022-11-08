83886dbe75c65262ab64596de67374bf5a6bda56
Abdur Rahman Emon <aremon86851@gmail.com>
2022-11-08 21:28:21 +0600

    get data by dervice id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // Middleware
@@ -11,16 +11,27 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.PASSWORD}@cluster0.hvqv2xi.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
         const servicesCollection = client.db('assignment11').collection('services');
-        app.get('/services', async (req, res) => {
+        app.get('/homeService', async (req, res) => {
             const query = {}
             const cursor = await servicesCollection.find(query)
             const services = await cursor.limit(3).toArray()
             res.send(services)
+        })
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = await servicesCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const service = await servicesCollection.findOne(query)
+            res.send(service)
         })
     }
     finally {
